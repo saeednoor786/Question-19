55be80e1ec8445b03af540d5b6a697ab02145008
Saeed Noor <163897168+saeednoor786@users.noreply.github.com>
2024-05-23 22:47:54 +0500

    nineteen assignment

---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+// Q:19 Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message 
+//  indicating the number of people you are inviting to dinner.
+let guests_list = ["saeed", "mubashir", "fareed"];
+console.log(`total number of people invite ${guests_list.length}`);
